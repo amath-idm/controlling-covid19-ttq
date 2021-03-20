ff1fe8936fed70a35d50d30e520a8c1ded6a275c
cliffckerr <questionably@gmail.com>
2021-03-19 23:51:35 -0700

    update fig 3

--- fig3_theoretical/run_fig3.py
+++ fig3_theoretical/run_fig3.py
@@ -6,55 +6,15 @@ The simulations take about 5 min to run.
 '''
 
 import numpy as np
-import pylab as pl
 import sciris as sc
 import covasim as cv
 
 # Basic setup
-do_run = 1
 do_save = 0
 simsfile = 'fig3.sims'
 T = sc.tic()
 s = sc.objdict()
 a = sc.objdict()
-
-# Figure configuration
-font_size = 22
-font_family = 'Proxima Nova'
-pl.rcParams['font.size'] = font_size
-pl.rcParams['font.family'] = font_family
-fig = pl.figure(figsize=(24,14))
-
-# Placeholder axes
-ay0 = 0.06
-adx = 0.2
-ady = 0.91
-axa, axb, axc = 0.03, 0.26, 0.49
-a['none']  = pl.axes([axa, ay0, adx, ady])
-a['test']  = pl.axes([axb, ay0, adx, ady])
-a['trace'] = pl.axes([axc, ay0, adx, ady])
-xoff = 0.02
-yoff = 0.05
-pl.figtext(axa-xoff, ady+yoff, 'A', fontsize=40)
-pl.figtext(axb-xoff, ady+yoff, 'B', fontsize=40)
-pl.figtext(axc-xoff, ady+yoff, 'C', fontsize=40)
-
-# For this figure
-adx = 0.21
-ady = 0.25
-hspace = 0.32
-aya, ayb, ayc = ay0+2*hspace, ay0+hspace, ay0
-axx = 0.77
-a['best'] = pl.axes([axx, aya, adx, ady])
-a['low']  = pl.axes([axx, ayb, adx, ady])
-a['high'] = pl.axes([axx, ayc, adx, ady])
-xoff = 0.05
-yoff = 0.26
-pl.figtext(axx-xoff, aya+yoff, 'D', fontsize=40)
-pl.figtext(axx-xoff, ayb+yoff, 'E', fontsize=40)
-pl.figtext(axx-xoff, ayc+yoff, 'F', fontsize=40)
-
-max_n = 0; ζ = {'↓':-2, '↑':10}
 
 
 #%% Configure for running
@@ -71,12 +31,12 @@ p = sc.objdict(
 
 # Overall settings
 betadict = dict(best=0.018, low=0.014, high=0.022)
-n_seeds = 10
+n_seeds = 10 # Number of replicates
 
 # Define scenarios
-sd = 10
-ed = 15
-bc = 1.0
+sd = 10 # Start day
+ed = 15 # End day
+bc = 1.0 # Beta change
 baseline_testing = cv.test_num(start_day=0, end_day=sd-1, daily_tests=p.pop_size/200)
 
 scenarios = dict(
@@ -98,8 +58,8 @@ scenarios = dict(
         ],
 )
 
-# Configure and run sims
-if do_run:
+
+if __name__ == '__main__':
 
     # Configure sims
     sims = []
@@ -116,49 +76,5 @@ if do_run:
     if do_save:
         cv.save(simsfile, sims)
 
-# Loading from disk
-else:
-    print(f'Loading {simsfile}...')
-    sims = cv.load(simsfile)
-
-for sim in sims:
-    s[sim.label] = sim
-
-
-#%% Plotting
-colors = dict(beta='#b22222', test='#80d380', trace='#6495ed')
-betamap = dict(best='Medium', low='Low', high='High')
-scenmap = dict(beta='Distancing', test='Testing', trace='TTQ')
-
-for betakey in betadict.keys():
-    for scenkey in scenarios.keys():
-        tvec = s[0].results['t']
-        average = np.zeros(s[0].npts)
-        for seed in np.arange(n_seeds):
-            label = f'{betakey}_{scenkey}_{seed}'
-            res = s[label].results
-            cum_infections = res['cum_infections'].values
-            a[betakey].plot(tvec, cum_infections, c=colors[scenkey], lw=3, alpha=0.2)
-            average += cum_infections/n_seeds
-        a[betakey].plot(tvec, average, '--', c=colors[scenkey], lw=3, alpha=1.0, zorder=10, label=scenmap[scenkey])
-
-for betakey in betadict.keys():
-    ax = a[betakey]
-    sc.commaticks(ax=ax)
-    sc.setylim(ax=ax)
-    ax.set_xlim([0,150])
-    ax.set_ylabel('Cumulative infections')
-    betanorm = betadict[betakey]*7*100 # Convert from a relative to absolute beta
-    ax.set_title(rf'{betamap[betakey]} transmission, $\beta$ = {betanorm:0.0f}%')
-    sc.boxoff(ax=ax)
-    if betakey == 'best':
-        ax.legend(frameon=False)
-    if betakey == 'high':
-        ax.set_yticks(np.arange(8)*2e3)
-        ax.set_xlabel('Days since seed infections')
-
-
-pl.show()
-
-print('Done.')
-sc.toc(T)
+    print('Done.')
+    sc.toc(T)
